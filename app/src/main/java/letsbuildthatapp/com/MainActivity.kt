6507c1915b3e5a954d3ef419c5014d6e8538a3a6
fehad <fehadaminkunroo@gmail.com>
2020-05-17 05:38:21 +0530

    Initial commit

---
+++ app/src/main/java/letsbuildthatapp/com/MainActivity.kt
@@ -0,0 +1,63 @@
+package letsbuildthatapp.com
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            performRegister()
+
+
+        }
+        already_have_account_text_view.setOnClickListener{
+            Log.d("MainActivity","Try to show Login activity")
+
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun performRegister(){
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty())
+        {
+            Toast.makeText(this,"Please Enter the Text in email and password",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity","Email is" +email)
+        Log.d("MainActivity","Password :  $password" )
+
+        //authenticate
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful)    return@addOnCompleteListener
+
+                //else if successful
+                Log.d("Main","Succesfully Created  user with uid : ${it.result!!.user!!.uid}")
+            }
+
+            .addOnFailureListener{
+                Log.d("Main","Failed to create User: ${it.message}")
+                Toast.makeText(this,"Failed to create User: ${it.message}",Toast.LENGTH_SHORT).show()
+
+            }
+
+    }
+}
+
